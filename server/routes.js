5844277b2e0621ae7acd9a9cc7332f4bd493167d
Fremen1990 <fremen1990@gmail.com>
2021-05-18 22:32:44 +0200

    error hadnling from API

--- server/routes.js
+++ server/routes.js
@@ -8,19 +8,33 @@ module.exports = function getRoutes() {
   //   res.status(200).json({ products });
   // });
 
+  // API ENDPOINT FOR ALL PRODUCT LIST
   router.get("/products", getProducts);
 
+  // ADDING ENDPOINT FOR SINGLE PRODUCT PER: ProductId
   router.get("/products/:productId", getProduct);
 
   return router;
 };
 
+// FUNCTION TO GET ALL PRODUCTS JSON
 function getProducts(req, res) {
   res.status(200).json({ products });
 }
 
+//FUNCTION TO GET ONE PRODUCT BY ProductId
 function getProduct(req, res) {
   const { productId } = req.params;
   const product = products.find((product) => product.id === productId);
-  res.status(200).json({ product });
+
+  try {
+    if (!product) {
+      throw Error(`No product found for id: ${productId}`);
+
+      // return res.status(404).json({ error });
+    }
+    res.status(200).json({ product });
+  } catch (error) {
+    return res.status(404).json({ statusCode: 404, message: error.message });
+  }
 }
